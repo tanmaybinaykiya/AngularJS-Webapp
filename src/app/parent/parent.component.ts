fa1b18aaccf63743570b258615e847742ce9029a
Tanmay Binaykiya <binaykiya.tanmay@gmail.com>
2016-06-10 10:25:25 +0530

    replacing mdl button with angular2-md button

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -5,7 +5,7 @@ import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { HeaderComponent } from '../header';
-
+import { MdButton, MdAnchor } from '@angular2-material/button/button';
 
 import { ParentService } from '../shared';
 import { ChildrenComponent } from './children';
@@ -14,7 +14,7 @@ import { ProfileComponent } from './profile';
 @Component({
     selector: 'my-parent',
     providers: [ParentService],
-    directives: [...ROUTER_DIRECTIVES, HeaderComponent, ...MD_SIDENAV_DIRECTIVES, ...MD_LIST_DIRECTIVES, ...MD_TOOLBAR_DIRECTIVES, ],
+    directives: [MdButton, MdAnchor, ...ROUTER_DIRECTIVES, HeaderComponent, ...MD_SIDENAV_DIRECTIVES, ...MD_LIST_DIRECTIVES, ...MD_TOOLBAR_DIRECTIVES, ],
     template: require('./parent.component.html'),
     styles: [require('./parent.component.scss')]
 })
