2dfd94f4439db2936d920a475034e073a7b0662a
Tanmay Binaykiya <binaykiya.tanmay@gmail.com>
2016-06-15 09:26:55 +0530

    Super admin

--- src/app/superadmin/superadmin.component.ts
+++ src/app/superadmin/superadmin.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card/card';
+
 @Component({
-  selector: 'my-superadmin',
-  template: require('./superadmin.component.html'),
-  styles: [require('./superadmin.component.scss')]
+    selector: 'my-superadmin',
+    template: require('./superadmin.component.html'),
+    styles: [require('./superadmin.component.scss')],
+    directives: [...MD_BUTTON_DIRECTIVES, ...MD_INPUT_DIRECTIVES, MD_CARD_DIRECTIVES],
 })
 export class SuperAdminComponent implements OnInit {
 
-  constructor() {
-    // Do stuff
-  }
+    constructor() {
+        // Do stuff
+    }
 
-  ngOnInit() {
-    console.log('Hello Super Admin');
-  }
+    ngOnInit() {
+        console.log('Hello Super Admin');
+    }
 
 }
