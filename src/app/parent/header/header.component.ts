83de050ccaed878830e39b488731c242104c1e18
Tanmay Binaykiya <binaykiya.tanmay@gmail.com>
2016-06-22 10:47:26 +0530

    logout button added

--- src/app/parent/header/header.component.ts
+++ src/app/parent/header/header.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService, SchoolService } from '../../shared';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { UserBadgeComponent } from './userBadge/user-badge.component';
+import { MdButton, MdAnchor } from '@angular2-material/button/button';
+
 @Component({
     selector: 'header-toolbar',
-    directives: [...MD_TOOLBAR_DIRECTIVES, UserBadgeComponent],
+    directives: [...MD_TOOLBAR_DIRECTIVES, UserBadgeComponent, MdButton, MdAnchor ],
     providers: [LoginService, SchoolService],
     template: require('./header.component.html'),
     styles: [require('./header.component.scss')]
